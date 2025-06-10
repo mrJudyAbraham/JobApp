6145ade1e8aa1d7313ca69c5cf09fd001289119a
Judy Abraham <mrjudyabraham@gmail.com>
2025-06-10 22:15:33 +0530

    added tailwind ui component for create page and need to edit it as per requirement

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,19 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
         // $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);        //searching for the job from list that matches the id
     // dd($job);
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
